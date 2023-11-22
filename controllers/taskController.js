0b4876e67709d9756bc0c93c00edac2e237a514b
mincTeja <himateja135@gmail.com>
2023-11-22 17:05:04 +0530

    addition of methods mentioned as part of assignment

--- controllers/taskController.js
+++ controllers/taskController.js
@@ -1,9 +1,36 @@
 const taskController = require('express').Router()
+const taskHelper = require('../helper/taskHelper')
 
 
 
 taskController.get('/', (req, res) => {
-    res.send('please find the tasks');
+    res
+    .send(taskHelper.fetchAllTasksInTaskList())
+    .status(200);
+})
+
+taskController.get('/:id', (req, res) => {
+    res
+    .send(taskHelper.fetchTaskInTaskList(req.params.id))
+    .status(200);
+})
+
+taskController.post('/', (req,res) => {
+    res
+    .send(taskHelper.addTaskInTaskList(req.body))
+    .status(200);
+})
+
+taskController.put('/:id', (req,res) => {
+    res
+    .send(taskHelper.updateTaskInTaskList(req.params.id, req.params.body))
+    .status(200);
+})
+
+taskController.delete('/:id', (req, res) => {
+    res
+    .send(taskHelper.deleteTaskInTaskList(req.params.id))
+    .status(200);
 })
 
 module.exports = taskController
